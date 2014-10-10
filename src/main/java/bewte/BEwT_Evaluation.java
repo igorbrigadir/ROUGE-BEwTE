afe0dc5e3f2b39caf7c12491f70becb414983108
igorbrigadir <igor.brigadir@gmail.com>
2014-10-10 10:27:46 +0000

    Replicate ant Build Eval in main

--- src/main/java/bewte/BEwT_Evaluation.java
+++ src/main/java/bewte/BEwT_Evaluation.java
@@ -3,8 +3,8 @@ package bewte;
 import tratz.runpipe.util.RunPipe;
 
 public class BEwT_Evaluation {
-	
-	static final String PATH = "/home/igor/BEwT/";
+		
+	static final String PATH = "/tmp/BEwT/";
 
 	static final String SUMMARIES_DIR = PATH+"eval";
 	
@@ -17,7 +17,6 @@ public class BEwT_Evaluation {
 	static final String STEP1_OUTPUT_DIR = INTERMEDIATE_FILES_DIR+"/parsed";
 	static final String STEP2_OUTPUT_DIR = INTERMEDIATE_FILES_DIR+"/BEs";
 	static final String STEP3_OUTPUT_DIR = INTERMEDIATE_FILES_DIR+"/BEXs";
-		
 	
 	static final String REFERENCE_NAME_PATTERN = "reference.*";
 
@@ -43,24 +42,24 @@ public class BEwT_Evaluation {
 		 * Step 1. Parse input documents
 		 */
 		System.out.println("Step 1. Parse input documents:");
-		//step1();		
-		System.out.println("Done...");
+		step1();		
+		System.out.println("Done.");
 		/*
 		 * Step 2. Extract BEs
 		 */
 		System.out.println("Step 2. Extract BEs");
-		//step2();
-		System.out.println("Done...");
+		step2();
+		System.out.println("Done.");
 		/*
 		 * Step 3. Perform Transformations
 		 */
 		System.out.println("Step 3. Perform Transformations");
-		//step3();
-		System.out.println("Done...");
+		step3();
+		System.out.println("Done.");
 		/*
 		 * Step 4. Perform Evaluation
 		 */
-		System.out.println(" Step 4. Perform Evaluation");
+		System.out.println("Step 4. Perform Evaluation");
 		step4();
 		
 		System.out.println("Finished.");
